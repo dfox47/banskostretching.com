f4fb673a9f3dcd6eaa048e6a8ed737cae03d672a
dfox <dfox@foxartbox.com>
2024-02-16 20:09:12 +0200

    Base 2

---
+++ template-parts/header.php
@@ -0,0 +1,18 @@
+<div class="header">
+	<div class="header_top">
+		<a class="header_top__link" href="//stores.aldi.us/stores"><span class="header_top__icon js-img-scroll" data-src="/i/icons/map.svg"></span> Find a Store</a>
+	</div>
+
+	<div class="header_bottom">
+		<a class="header_logo" href="//www.aldi.us/" data-ya-track="logo" title="Go to ALDI home page"><img class="Header-logo" src="//dynl.mktgcdn.com/p/wy61XJBkY69s1bUroTy87O_lqcz6I9BdfQ6vW-9UMeM/283x340.png" alt="ALDI Logo"></a>
+
+		<ul class="header_links">
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/products/">Products</a></li>
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/weekly-specials/our-weekly-ads/">Weekly Ad</a></li>
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/weekly-specials/this-weeks-aldi-finds/">ALDI Finds</a></li>
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/online-shopping/grocery-delivery/">Grocery Delivery</a></li>
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/online-shopping/grocery-pickup/">Grocery Pickup</a></li>
+			<li class="header_links__item"><a class="header_links__link" href="//www.aldi.us/recipes/">Recipes</a></li>
+		</ul>
+	</div>
+</div>
