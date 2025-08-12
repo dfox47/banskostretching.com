deab0c8c145f54077455713e4b3c7e8e1c34c7cf
Denis FOX <dfox@foxartbox.com>
2025-08-12 15:23:07 +0300

    plugins updates

---
+++ wp-content/plugins/tutor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '88a77501bcb5968ff6a4d11b0b87770e6cf3fd6a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '88a77501bcb5968ff6a4d11b0b87770e6cf3fd6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
