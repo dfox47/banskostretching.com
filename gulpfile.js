27bbc9189a811b72b13e609bc9a5e52fb03aa332
Denis FOX <dfox@foxartbox.com>
2025-11-02 19:52:32 +0200

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,13 @@ let remoteTheme           = '/public_html/wp-content/themes/banskostretching/'
 let remoteCss             = remoteTheme + 'css/'
 let remoteJs              = remoteTheme + 'js/'
 let remoteTemplateParts   = remoteTheme + 'template-parts/'
+let remoteIncludes        = remoteTheme + 'includes/'
 
 let localTheme            = 'wp-content/themes/banskostretching/'
 let localCss              = localTheme + 'css/'
 let localJs               = localTheme + 'js/'
 let localTemplateParts    = localTheme + 'template-parts/'
+let localIncludes         = localTheme + 'includes/'
 
 function getFtpConnection() {
   return ftp.create({
@@ -44,12 +46,6 @@ let conn = getFtpConnection()
 
 gulp.task('copy_html', function () {
   return gulp.src(localTheme + '*.php').pipe(conn.dest(remoteTheme))
-})
-
-gulp.task('copy_template_parts', function () {
-  return gulp
-    .src(localTemplateParts + '**/*')
-    .pipe(conn.dest(remoteTemplateParts))
 })
 
 gulp.task('css', function () {
@@ -69,15 +65,15 @@ gulp.task('css', function () {
 })
 
 gulp.task('copy_css', function () {
-  return gulp
-    .src(localCss + '**/*')
-    .pipe(conn.dest(remoteCss))
+  return gulp.src(localCss + '**/*').pipe(conn.dest(remoteCss))
+})
+
+gulp.task('copy_includes', function () {
+  return gulp.src(localIncludes + '**/*').pipe(conn.dest(remoteIncludes))
 })
 
 gulp.task('copy_js', function () {
-  return gulp
-    .src(localJs + '**/*.js')
-    .pipe(conn.dest(remoteJs))
+  return gulp.src(localJs + '**/*.js').pipe(conn.dest(remoteJs))
 })
 
 gulp.task('js', function () {
@@ -97,11 +93,16 @@ gulp.task('js', function () {
   )
 })
 
+gulp.task('copy_template_parts', function () {
+  return gulp.src(localTemplateParts + '**/*').pipe(conn.dest(remoteTemplateParts))
+})
+
 gulp.task('watch', function () {
-  gulp.watch(localTheme + '*.php',            gulp.series('copy_html'))
   gulp.watch(localCss + '**/*',               gulp.series('css', 'copy_css'))
+  gulp.watch(localIncludes + '**/*',          gulp.series('copy_includes'))
   gulp.watch(localJs + '**/*.js',             gulp.series('js', 'copy_js'))
   gulp.watch(localTemplateParts + '**/*',     gulp.series('copy_template_parts'))
+  gulp.watch(localTheme + '*.php',            gulp.series('copy_html'))
 })
 
 gulp.task('default', gulp.series('watch'))
