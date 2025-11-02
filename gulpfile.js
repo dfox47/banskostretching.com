2ff53b1e24c00f82d2c2f87e2f742f14ef524452
Denis FOX <dfox@foxartbox.com>
2025-11-02 19:49:35 +0200

    fix. Part 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,107 @@
-let cssMinify = require("gulp-csso");
-let concat = require("gulp-concat");
-let ftp = require("vinyl-ftp");
-let gulp = require("gulp");
-let gutil = require("gulp-util");
-let rename = require("gulp-rename");
-// let sass                = require('gulp-sass');
-let sass = require("gulp-sass")(require("sass"));
-let uglify = require("gulp-uglify");
+// npm install -g gulp-cli
+// npm install gulp vinyl-ftp gulp-concat gulp-csso gulp-rename gulp-sass gulp-uglify --save-dev
+
+const fs            = require('fs')
+const config        = JSON.parse(fs.readFileSync('../config.json'))
+let cssMinify       = require('gulp-csso')
+let concat          = require('gulp-concat')
+let ftp             = require('vinyl-ftp')
+let gulp            = require('gulp')
+let gutil           = require('gulp-util')
+let rename          = require('gulp-rename')
+// let sass                = require('gulp-sass')
+let sass            = require('gulp-sass')(require('sass'))
+let uglify          = require('gulp-uglify')
 
 // FTP config
-let user = "u608786266.banskostretching.com";
-let password = "b_eTVP$7U-'%z}qx.Sf])d";
-let host = "195.35.49.50";
-let port = 21;
+const host          = config.host
+const password      = config.password
+const port          = config.port
+const user          = config.user
 
-let remoteFolder = "/public_html/wp-content/themes/banskostretching/";
-let remoteFolderCss = remoteFolder + "css/";
-let remoteFolderJs = remoteFolder + "js/";
+let remoteTheme           = '/public_html/wp-content/themes/banskostretching/'
+let remoteCss             = remoteTheme + 'css/'
+let remoteJs              = remoteTheme + 'js/'
+let remoteTemplateParts   = remoteTheme + 'template-parts/'
 
-let localFolder = "wp-content/themes/banskostretching/";
-let localFolderCss = localFolder + "css/";
-let localFolderJs = localFolder + "js/";
+let localTheme            = 'wp-content/themes/banskostretching/'
+let localCss              = localTheme + 'css/'
+let localJs               = localTheme + 'js/'
+let localTemplateParts    = localTheme + 'template-parts/'
 
 function getFtpConnection() {
   return ftp.create({
     host: host,
     log: gutil.log,
     password: password,
-    parallel: 1,
+    parallel: 3,
     port: port,
     user: user,
   });
 }
 
-let conn = getFtpConnection();
+let conn = getFtpConnection()
 
-gulp.task("copy_html", function () {
-  return gulp.src(localFolder + "*.php").pipe(conn.dest(remoteFolder));
-});
+gulp.task('copy_html', function () {
+  return gulp.src(localTheme + '*.php').pipe(conn.dest(remoteTheme))
+})
 
-gulp.task("copy_template_parts", function () {
-  return gulp.src(localFolder + "template-parts/**/*").pipe(conn.dest(remoteFolder + "template-parts/"));
-});
-
-gulp.task("css", function () {
+gulp.task('copy_template_parts', function () {
   return gulp
-    .src(localFolderCss + "styles.scss")
+    .src(localTemplateParts + '**/*')
+    .pipe(conn.dest(remoteTemplateParts))
+})
+
+gulp.task('css', function () {
+  return gulp
+    .src(localCss + 'styles.scss')
     .pipe(sass())
     .pipe(cssMinify())
-   
     .pipe(
       rename({
-        basename: "styles",
+        basename: 'styles',
       })
     )
     .pipe(rename({
-      suffix: ".min"
+      suffix: '.min'
     }))
-    .pipe(conn.dest(remoteFolder));
-});
+    .pipe(conn.dest(remoteTheme))
+})
 
-gulp.task("copy_css", function () {
-  return gulp.src(localFolderCss + "**/*").pipe(conn.dest(remoteFolderCss));
-});
+gulp.task('copy_css', function () {
+  return gulp
+    .src(localCss + '**/*')
+    .pipe(conn.dest(remoteCss))
+})
 
-gulp.task("copy_js", function () {
-  return gulp.src(localFolderJs + "**/*.js").pipe(conn.dest(remoteFolderJs));
-});
+gulp.task('copy_js', function () {
+  return gulp
+    .src(localJs + '**/*.js')
+    .pipe(conn.dest(remoteJs))
+})
 
-gulp.task("js", function () {
+gulp.task('js', function () {
   return (
     gulp
       .src([
-        localFolderJs + "**/*.js",
+        localJs + '**/*.js',
       ])
-      .pipe(concat("all.js"))
+      .pipe(concat('all.js'))
       // .pipe(uglify())
       .pipe(
         rename({
-          suffix: ".min",
+          suffix: '.min',
         })
       )
-      .pipe(conn.dest(remoteFolder))
-  );
-});
+      .pipe(conn.dest(remoteTheme))
+  )
+})
 
-gulp.task("watch", function () {
-  gulp.watch(localFolder + "*.php", gulp.series("copy_html"));
-  gulp.watch(localFolderCss + "**/*", gulp.series("css", "copy_css"));
-  gulp.watch(localFolderJs + "**/*.js", gulp.series("js", "copy_js"));
-  gulp.watch(localFolder + "template-parts/**/*", gulp.series("copy_template_parts"));
-});
+gulp.task('watch', function () {
+  gulp.watch(localTheme + '*.php',            gulp.series('copy_html'))
+  gulp.watch(localCss + '**/*',               gulp.series('css', 'copy_css'))
+  gulp.watch(localJs + '**/*.js',             gulp.series('js', 'copy_js'))
+  gulp.watch(localTemplateParts + '**/*',     gulp.series('copy_template_parts'))
+})
 
-gulp.task("default", gulp.series("watch"));
+gulp.task('default', gulp.series('watch'))
