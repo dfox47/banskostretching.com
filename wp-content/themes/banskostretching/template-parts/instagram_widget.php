2e1e4ad0827eb5e4fdb92707581ccedcd66719e9
Denis FOX <dfox@foxartbox.com>
2025-11-28 22:03:09 +0200

    insta custom widget

--- wp-content/themes/banskostretching/template-parts/instagram_widget.php
+++ wp-content/themes/banskostretching/template-parts/instagram_widget.php
@@ -1,6 +1,73 @@
-<div class="container">
+<?php // vars
+$themeDir   = '/wp-content/themes/banskostretching';
+$i          = $themeDir . '/i';
+$instaDir   = $i . '/insta'; ?>
+
+<!-- <div class="container">
   <div class="ig_wrap">
     <script src="//static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
     <div class="elfsight-app-7bc4a5c0-741f-4916-b5ae-90bb8549b8fb" data-elfsight-app-lazy></div>
   </div>
+</div> -->
+
+<div class="wrap">
+  <div class="insta_header">
+    <a class="insta_header__img_link" href="//www.instagram.com/stretching_nelly" target="_blank"><img class="insta_header__img" src="<?= $instaDir; ?>/main.jpg" alt=""></a>
+
+    <div class="insta_header__wrap">
+      <a class="insta_header__link" href="//www.instagram.com/stretching_nelly" target="_blank">stretching_nelly</a>
+
+      <div class="insta_header__desc">Неля | Стретчинг | Barre | Фітнес | Онлайн</div>
+    </div>
+  </div>
+
+  <ul class="insta">
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DRmuILmDGJl/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/1.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DRZUXrqDOHh/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/2.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DRR3rlDjGcz/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/3.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DRRRS7xjGx6/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/4.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DRFp1lOiJ9h/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/5.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DQ3wRk_jJGQ/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/6.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DQ2Ri_XDLir/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/7.jpg" alt="">
+      </a>
+    </li>
+
+    <li class="insta__item">
+      <a class="insta__link" href="//www.instagram.com/p/DQhJMG_jAJu/" target="_blank">
+        <img class="insta__img" src="<?= $instaDir; ?>/8.jpg" alt="">
+      </a>
+    </li>
+  </ul>
 </div>
